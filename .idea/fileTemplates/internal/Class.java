a29b56150641100a8789ab08a4f879758bd296c2
Aemogie <theaemogie@gmail.com>
2021-03-25 16:04:22 +0530

    Delete .idea directory

--- .idea/fileTemplates/internal/Class.java
+++
@@ -1,7 +0,0 @@
-#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
-#parse("File Header.java")
-/**
- * @author <a href="mailto:theaemogie@gmail.com?subject=%5Blearn-mod_1.0%5D"> Aemogie. </a>
- */
-public class ${NAME} {
-}
