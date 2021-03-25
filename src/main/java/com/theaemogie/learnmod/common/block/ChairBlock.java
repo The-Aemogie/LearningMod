9b9a7bf87927161f57b7762fdbf19e9446c09c38
Aemogie <54412618+The-Aemogie@users.noreply.github.com>
2021-03-25 15:34:31 +0530

    Abstracted ChairBlock to AbstractSeatBlock and interfaces IRotatable, ISittable (Need a better name.), IDoubleBlock.

--- src/main/java/com/theaemogie/learnmod/common/block/ChairBlock.java
+++ src/main/java/com/theaemogie/learnmod/common/block/ChairBlock.java
@@ -1,50 +1,40 @@
 package com.theaemogie.learnmod.common.block;
 
-import com.theaemogie.learnmod.common.block.voxels.ChairVoxel;
-import com.theaemogie.learnmod.common.entity.SeatEntity;
+import com.theaemogie.learnmod.common.block.interfaces.IDoubleBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
-import net.minecraft.state.DirectionProperty;
-import net.minecraft.state.EnumProperty;
 import net.minecraft.state.StateContainer;
-import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.state.properties.DoubleBlockHalf;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
-import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
 
-import java.util.Arrays;
-import java.util.Objects;
+import static com.theaemogie.learnmod.common.block.voxels.ChairVoxel.Occlusion;
 
-public class ChairBlock extends Block {
-
-    public static DirectionProperty HORIZONTAL_FACING = BlockStateProperties.HORIZONTAL_FACING;
-    public static EnumProperty<DoubleBlockHalf> HALF = BlockStateProperties.DOUBLE_BLOCK_HALF;
-    private static BlockPos offset = new BlockPos(0.5, 0.75, 0.5);
+public class ChairBlock extends AbstractSeatBlock implements IDoubleBlock {
 
     public ChairBlock() {
-        super(Properties.of(Material.WOOD)
-                .sound(SoundType.WOOD)
-                .strength(2.0f)
-                .harvestTool(ToolType.AXE)
-                .noOcclusion()
+        super(
+                Properties
+                        .of(Material.WOOD)
+                        .sound(SoundType.WOOD)
+                        .strength(2.0f)
+                        .harvestTool(ToolType.AXE),
+                0.75d
         );
         this.registerDefaultState(this.stateDefinition.any()
                 .setValue(HORIZONTAL_FACING, Direction.NORTH)
@@ -52,145 +42,60 @@ public class ChairBlock extends Block {
         );
     }
 
-    protected static void preventCreativeDropFromBottomPart(World world, BlockPos pos, BlockState state, PlayerEntity player) {
-
-        DoubleBlockHalf doubleblockhalf = state.getValue(HALF);
-        BlockPos secondPos;
-
-        if (doubleblockhalf == DoubleBlockHalf.UPPER) {
-            secondPos = pos.below();
-        } else {
-            secondPos = pos.above();
-        }
-
-        BlockState secondState = world.getBlockState(secondPos);
-
-        if (secondState.getBlock() == state.getBlock() && secondState.getValue(HALF) != state.getValue(HALF)) {
-            world.setBlock(secondPos, Blocks.AIR.defaultBlockState(), 35);
-            world.levelEvent(player, 2001, secondPos, Block.getId(secondState));
-        }
-    }
-
-    @SuppressWarnings({"deprecation", "NullableProblems"})
-    @Override
-    public VoxelShape getOcclusionShape(BlockState state, IBlockReader world, BlockPos pos) {
-        return occlusionShapeProvider(state.getValue(HORIZONTAL_FACING), state.getValue(HALF));
-    }
-
-    @SuppressWarnings({"deprecation", "NullableProblems"})
-    @Override
-    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-        return occlusionShapeProvider(state.getValue(HORIZONTAL_FACING), state.getValue(HALF));
-    }
-
-    private VoxelShape occlusionShapeProvider(Direction direction, DoubleBlockHalf half) {
-        if (half == DoubleBlockHalf.LOWER) {
-            switch (direction) {
-                case NORTH:
-                    return shapeBuilder(ChairVoxel.COMMON_LOWER, ChairVoxel.Occlusion.NORTH_LOWER);
-                case EAST:
-                    return shapeBuilder(ChairVoxel.COMMON_LOWER, ChairVoxel.Occlusion.EAST_LOWER);
-                //                return Rotate.rotateShape(Direction.NORTH, Direction.EAST, ChairVoxel.GET);
-                case SOUTH:
-                    return shapeBuilder(ChairVoxel.COMMON_LOWER, ChairVoxel.Occlusion.SOUTH_LOWER);
-                //                return Rotate.rotateShape(Direction.NORTH, Direction.SOUTH, ChairVoxel.GET);
-                case WEST:
-                    return shapeBuilder(ChairVoxel.COMMON_LOWER, ChairVoxel.Occlusion.WEST_LOWER);
-                default:
-                    return null;
-            }
-        } else {
-            switch (direction) {
-                case NORTH:
-                    return shapeBuilder(ChairVoxel.COMMON_UPPER, ChairVoxel.Occlusion.NORTH_UPPER);
-                case EAST:
-                    return shapeBuilder(ChairVoxel.COMMON_UPPER, ChairVoxel.Occlusion.EAST_UPPER);
-                //                return Rotate.rotateShape(Direction.NORTH, Direction.EAST, ChairVoxel.GET);
-                case SOUTH:
-                    return shapeBuilder(ChairVoxel.COMMON_UPPER, ChairVoxel.Occlusion.SOUTH_UPPER);
-                //                return Rotate.rotateShape(Direction.NORTH, Direction.SOUTH, ChairVoxel.GET);
-                case WEST:
-                    return shapeBuilder(ChairVoxel.COMMON_UPPER, ChairVoxel.Occlusion.WEST_UPPER);
-                default:
-                    return null;
-            }
-        }
-    }
-    
-    private VoxelShape collisionShapeProvider(Direction direction, DoubleBlockHalf half) {
-        if (half == DoubleBlockHalf.LOWER) {
-            switch (direction) {
-                case NORTH:
-                    return shapeBuilder(ChairVoxel.COMMON_LOWER, ChairVoxel.Collision.NORTH_LOWER);
-                case EAST:
-                    return shapeBuilder(ChairVoxel.COMMON_LOWER, ChairVoxel.Collision.EAST_LOWER);
-                //                return Rotate.rotateShape(Direction.NORTH, Direction.EAST, ChairVoxel.GET);
-                case SOUTH:
-                    return shapeBuilder(ChairVoxel.COMMON_LOWER, ChairVoxel.Collision.SOUTH_LOWER);
-                //                return Rotate.rotateShape(Direction.NORTH, Direction.SOUTH, ChairVoxel.GET);
-                case WEST:
-                    return shapeBuilder(ChairVoxel.COMMON_LOWER, ChairVoxel.Collision.WEST_LOWER);
-                default:
-                    return null;
-            }
-        } else {
-            switch (direction) {
-                case NORTH:
-                    return shapeBuilder(ChairVoxel.COMMON_UPPER, ChairVoxel.Collision.NORTH_UPPER);
-                case EAST:
-                    return shapeBuilder(ChairVoxel.COMMON_UPPER, ChairVoxel.Collision.EAST_UPPER);
-                //                return Rotate.rotateShape(Direction.NORTH, Direction.EAST, ChairVoxel.GET);
-                case SOUTH:
-                    return shapeBuilder(ChairVoxel.COMMON_UPPER, ChairVoxel.Collision.SOUTH_UPPER);
-                //                return Rotate.rotateShape(Direction.NORTH, Direction.SOUTH, ChairVoxel.GET);
-                case WEST:
-                    return shapeBuilder(ChairVoxel.COMMON_UPPER, ChairVoxel.Collision.WEST_UPPER);
-                default:
-                    return null;
-            }
-        }
-    }
-
-    @SuppressWarnings("OptionalGetWithoutIsPresent")
-    private VoxelShape shapeBuilder(VoxelShape... shapes) {
-        return Arrays.stream(shapes).reduce(
-                (v1, v2) -> VoxelShapes.join(v1, v2, IBooleanFunction.OR)
-        ).get();
-    }
-
-    @SuppressWarnings("NullableProblems")
     @Override
     protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
         super.createBlockStateDefinition(builder);
-        builder.add(HORIZONTAL_FACING);
         builder.add(HALF);
     }
 
     @Override
-    public BlockState getStateForPlacement(BlockItemUseContext context) {
-        BlockPos pos = context.getClickedPos();
-        World world = context.getLevel();
-        int worldHeight = world.dimensionType().logicalHeight() - 1;
-        BlockState blockAbove = world.getBlockState(pos.above());
-        if (pos.getY() < worldHeight) {
-            if (blockAbove.canBeReplaced(context)) {
-                return this.defaultBlockState().setValue(HORIZONTAL_FACING, context.getHorizontalDirection().getOpposite());
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        DoubleBlockHalf half = state.getValue(HALF);
+        Direction direction = state.getValue(HORIZONTAL_FACING);
+        if (half == DoubleBlockHalf.LOWER) {
+            switch (direction) {
+                case NORTH:
+                    return Occlusion.NORTH_LOWER_FULL;
+                case EAST:
+                    return Occlusion.EAST_LOWER_FULL;
+                //                return Rotate.rotateShape(Direction.NORTH, Direction.EAST, ChairVoxel.GET);
+                case SOUTH:
+                    return Occlusion.SOUTH_LOWER_FULL;
+                //                return Rotate.rotateShape(Direction.NORTH, Direction.SOUTH, ChairVoxel.GET);
+                case WEST:
+                    return Occlusion.WEST_LOWER_FULL;
+                default:
+                    return null;
+            }
+        } else {
+            switch (direction) {
+                case NORTH:
+                    return Occlusion.NORTH_UPPER_FULL;
+                case EAST:
+                    return Occlusion.EAST_UPPER_FULL;
+                //                return Rotate.rotateShape(Direction.NORTH, Direction.EAST, ChairVoxel.GET);
+                case SOUTH:
+                    return Occlusion.SOUTH_UPPER_FULL;
+                //                return Rotate.rotateShape(Direction.NORTH, Direction.SOUTH, ChairVoxel.GET);
+                case WEST:
+                    return Occlusion.WEST_UPPER_FULL;
+                default:
+                    return null;
             }
         }
-        return null;
     }
 
-    @SuppressWarnings("NullableProblems")
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return canPlace(context, super.getStateForPlacement(context));
+    }
+
     @Override
     public void playerWillDestroy(World world, BlockPos pos, BlockState state, PlayerEntity player) {
-        if (!world.isClientSide && player.isCreative()) {
-            preventCreativeDropFromBottomPart(world, pos, state, player);
-        }
-
+        preventDropFromOppositePart(world, pos, state, player);
         super.playerWillDestroy(world, pos, state, player);
     }
 
-    @SuppressWarnings("deprecation")
     @Override
     public boolean canSurvive(BlockState state, IWorldReader world, BlockPos pos) {
         BlockPos blockpos = pos.below();
@@ -203,25 +108,17 @@ public class ChairBlock extends Block {
         }
     }
 
-    @SuppressWarnings("NullableProblems")
     @Override
     public void setPlacedBy(World world, BlockPos pos, BlockState blockState, LivingEntity entity, ItemStack blockItemStack) {
-        world.setBlock(pos.above(), this.defaultBlockState()
-                        .setValue(HALF, DoubleBlockHalf.UPPER)
-                        .setValue(HORIZONTAL_FACING, Objects.requireNonNull(entity).getDirection().getOpposite())
-                , 3
-        );
+        setOppositeHalf(world, pos, this.defaultBlockState().setValue(HORIZONTAL_FACING, getRotation(entity)));
     }
 
     @Override
-    @SuppressWarnings({"deprecation", "NullableProblems"})
-    public ActionResultType use(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
-        if (state.getValue(HALF) == DoubleBlockHalf.UPPER) {
-            offset = new BlockPos(
-                    offset.getX(),
-                    offset.getY() - 1,
-                    offset.getZ());
+    public ActionResultType use(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult subBlockPos) {
+        DoubleBlockHalf half = state.getValue(HALF);
+        if (half == DoubleBlockHalf.UPPER) {
+            pos = pos.below();
         }
-        return SeatEntity.create(world, pos, offset, player);
+        return super.use(state, world, pos, player, hand, subBlockPos);
     }
 }
