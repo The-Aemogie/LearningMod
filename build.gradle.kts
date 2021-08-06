94e31a8816849210f99d52f0736d43348e026201
aemogie <theaemogie@gmail.com>
2021-08-06 20:17:15 +0530

    :rocket: my gradle is now cooler than urs.

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+import net.minecraftforge.gradle.userdev.UserDevExtension
+
+val mappingsChannel: String by extra
+val mappingsVersion: String by extra
+val forgeVersion: String by extra
+
+buildscript {
+	repositories { maven("https://files.minecraftforge.net/maven") }
+	dependencies { classpath("net.minecraftforge.gradle:ForgeGradle:+") }
+}
+
+plugins {
+	java
+	kotlin("jvm") version "1.5.21"
+}
+
+apply {
+	plugin("net.minecraftforge.gradle")
+}
+
+version = "1.0"
+group = "io.github.aemogie"
+
+configure<UserDevExtension> {
+	mappings(mappingsChannel, mappingsVersion)
+	runs {
+		create("client") {
+			workingDirectory(project.file(".run"))
+			property("forge.logging.markers", "REGISTRIES")
+			property("forge.logging.console.level", "debug")
+			mods { sources.forEach { create(it.name) {source(it)} } }
+		}
+	}
+}
+
+dependencies {
+	"minecraft"("net.minecraftforge:forge:$forgeVersion")
+	implementation("io.github.spair:imgui-java-app:+")
+	implementation(kotlin("stdlib-jdk8"))
+}
+tasks.withType<Jar> {
+	manifest {
+		attributes["Specification-Title"] = "Learning Mod"
+		attributes["Specification-Vendor"] = "aemogie."
+		attributes["Specification-Version"] = project.version
+		attributes["Implementation-Title"] = project.name
+		attributes["Implementation-Version"] = archiveVersion
+		attributes["Implementation-Vendor"] = "aemogie."
+	}
+}
